d2b38678ec31409a29187551b570cfd929864b96
Isabella Reis <isabellanery15@hotmail.com>
2023-01-20 11:29:54 -0300

    Adicioando componente: Banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { BannerContainer } from "./styles/Banner";
+
+function Banner() {
+  return (
+    <BannerContainer>
+      <img
+        src="/imagens/banner.png"
+        alt="Banner principal da página do Organo."
+      />
+    </BannerContainer>
+  );
+}
+
+export default Banner;
---
+++ src/componentes/Banner/styles/Banner.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const BannerContainer = styled.header`
+    background-color: #6278f7;
+    text-align: center;
+
+    img {
+        max-width: 100%;
+    }
+`
